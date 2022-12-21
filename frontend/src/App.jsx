49a9d080283f3d718b7383f164acc5c0b771ae88
Carlos Serôdio <carlosserodio86@hotmail.com>
2022-12-21 09:39:40 +0000

    Install React Router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+// [] {}
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </div>
+    </BrowserRouter>
   );
 };
 
